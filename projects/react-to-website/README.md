3c8b79115bd9294a6944afd1006da5e76fec4973
rmanuel7 <manuel7@outlook.es>
2024-06-10 12:04:56 -0500

    I have just added the first React component to my website without jsx

---
+++ projects/react-to-website/README.md
@@ -0,0 +1,7 @@
+# Add React to a Website
+
+Add a React component to an existing HTML page.
+
+- [x] Step 1: Add a DOM Container to the HTML
+- [x] Step 2: Add the Script Tags
+- [x] Step 3: Create a React Component
---
+++ projects/react-to-website/src/main.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+
+import LikeButton from './LikeButton';
+
+const domContainer = document.querySelector('#app');
+
+const root = ReactDOM.createRoot(domContainer);
+
+root.render(React.createElement(LikeButton));

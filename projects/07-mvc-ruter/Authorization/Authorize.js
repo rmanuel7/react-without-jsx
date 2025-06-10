ba289f47eccd4ea42b482c17385faa74cb052b7f
rmanuel7 <manuel7@outlook.es>
2025-06-10 14:50:32 -0500

    Update Authorize.js

--- projects/07-mvc-ruter/Authorization/Authorize.js
+++ projects/07-mvc-ruter/Authorization/Authorize.js
@@ -1,22 +1,7 @@
 
 class Authorize extends React.Component {
     render() {
-        return h({
-            type: AuthzContext.Provider,
-            props: {
-                value: {
-                    type: 'authorize',
-                    config: {
-                        roles: this.props.roles || null,
-                        policy: this.props.policy || null,
-                        claims: this.props.claims || null,
-                    }
-                }
-            },
-            children: [
-                h({ type: Outlet })
-            ]
-        });
+        return null;
     }
 }
 

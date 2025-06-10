a55f337f9fe4ad1bbf3b3f0a278c37d352d03b5f
rmanuel7 <manuel7@outlook.es>
2025-06-10 07:55:17 -0500

    Update Outlet.js

--- projects/07-mvc-ruter/Router/Outlet.js
+++ projects/07-mvc-ruter/Router/Outlet.js
@@ -1,1 +1,13 @@
 // Outlet solo consume el contexto que está “más cerca”.
+import React from "react";
+import OutletContext  from "./OutletContext";
+
+class Outlet extends React.Component {
+    render() {
+        return React.createElement(OutletContext.Consumer, {}, outletContext => {
+            return outletContext && outletContext.outlet ? outletContext.outlet : null;
+        });
+    }
+}
+
+export default Outlet;

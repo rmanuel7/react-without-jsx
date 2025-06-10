715835bd57c44955184241cf9c162bb1c82adbd6
rmanuel7 <manuel7@outlook.es>
2025-06-10 05:50:59 -0500

    Create Route.js

---
+++ projects/07-mvc-ruter/Router/Route.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Route extends React.Component {
+    render() {
+        // No renderiza nada (estructura declarativa solamente)
+        return null;
+    }
+}
+
+export default Route;

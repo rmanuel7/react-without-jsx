e8ad08f67d87a067debe194c14e49de47f3e197b
rmanuel7 <manuel7@outlook.es>
2025-06-10 14:31:18 -0500

    Create withAuthz.js

---
+++ projects/07-mvc-ruter/Authorization/withAuthz.js
@@ -0,0 +1,33 @@
+import AuthzContext from './AuthzContext.js';
+import AuthContext from '../Authentication/AuthContext.js';
+import withAuth from '../Authentication/withAuth.js';
+import { createReactElement as h} from '../Shared/ReactFunctions.js';
+
+function withAuthz(Component) {
+    return class extends React.Component {
+        render() {
+            return h({
+                type: AuthContext.Consumer,
+                props: {},
+                children: [auth =>
+                    h({
+                        type: AuthzContext.Consumer,
+                        props: {},
+                        children: [authz =>
+                            h({
+                                type: Component,
+                                props: {
+                                    ...this.props,
+                                    authz,
+                                    user: auth.user
+                                }
+                            })
+                        ]
+                    })
+                ]
+            });
+        }
+    };
+};
+
+export default withAuthz;

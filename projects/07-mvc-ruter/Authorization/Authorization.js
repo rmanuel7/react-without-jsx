e1949ca4325038bbfb8a0e81f71f9fdeba2f37e8
rmanuel7 <manuel7@outlook.es>
2025-06-10 15:00:08 -0500

    Update Authorization.js

--- projects/07-mvc-ruter/Authorization/Authorization.js
+++ projects/07-mvc-ruter/Authorization/Authorization.js
@@ -3,32 +3,29 @@ import Outlet from '../Router/Outlet.js';
 import AccessDenied from './AccessDenied.js';
 import { createReactElement as h } from '../Wrappers/ReactFunctions.js';
 
-const Authorization = withAuthz(
-    class Authorization extends React.Component {
-        render() {
-            const { user, authz, children } = this.props;
+class AuthorizationBase extends React.Component {
+    render() {
+        const { user, authz, children } = this.props;
 
-            if (this.isAuthorized(user, authz.config)) {
-                return h({ type: Outlet });
-            }
-
-            return h({ type: AccessDenied });
+        if (this.isAuthorized(user, authz?.config || {})) {
+            return h({ type: Outlet });
         }
 
-        isAuthorized(user, config) {
-            const { roles, policy, claims } = config;
-
-            if (!user.isAuthenticated) return false;
-
-            if (roles && !roles.split(',').some(role => user.roles.includes(role.trim()))) {
-                return false;
-            }
-
-            // Implementa aquí lógica para policy y claims si lo necesitas
-
-            return true;
-        }
+        return h({ type: AccessDenied });
     }
-);
 
-export default Authorization;
+    isAuthorized(user = {}, config = {}) {
+        const { roles, policy, claims } = config;
+        if (!user.isAuthenticated) return false;
+
+        if (roles && !roles.split(',').some(role => user.roles.includes(role.trim()))) {
+            return false;
+        }
+
+        // Aquí puedes implementar lógica para policy y claims
+
+        return true;
+    }
+}
+
+export default withAuthz(AuthorizationBase);

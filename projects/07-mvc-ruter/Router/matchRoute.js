44b00c7129e5c61e46f360091cf52a0af18a7ad7
rmanuel7 <manuel7@outlook.es>
2025-06-10 07:41:56 -0500

    Create matchRoute.js

---
+++ projects/07-mvc-ruter/Router/matchRoute.js
@@ -0,0 +1,36 @@
+function matchRoute(routes, pathname) {
+    // Quita trailing slashes para uniformidad
+    const cleanPath = pathname.replace(/\/+$/, '') || '/';
+
+    for (let route of routes) {
+        // Soporte exacto
+        if (route.path === cleanPath) {
+            return { route, params: {} };
+        }
+        // Soporte básico para parámetros tipo /home/about/:id
+        // route.path: "/home/about/:id"
+        // pathname:   "/home/about/42"
+        const routeParts = route.path.split('/');
+        const pathParts = cleanPath.split('/');
+        if (routeParts.length === pathParts.length) {
+            let params = {};
+            let match = true;
+            for (let i = 0; i < routeParts.length; i++) {
+                if (routeParts[i].startsWith(':')) {
+                    const paramName = routeParts[i].slice(1);
+                    params[paramName] = pathParts[i];
+                } else if (routeParts[i] !== pathParts[i]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return { route, params };
+            }
+        }
+    }
+    // No match found
+    return { route: null, params: {} };
+}
+
+export default matchRoute;

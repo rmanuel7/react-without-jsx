76e961f278136269924078bf731a9ed68b1acd21
rmanuel7 <manuel7@outlook.es>
2025-06-10 13:17:02 -0500

    Update and rename ActionResultContext.js to ActionContext.js

---
+++ projects/07-mvc-ruter/Controllers/ActionContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ActionContext = React.createContext(null);
+
+export default ActionContext;
--- projects/07-mvc-ruter/Controllers/ActionResultContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const ActionResultContext = React.createContext(null);
-
-export default ActionResultContext;

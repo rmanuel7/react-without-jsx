54a8d749a43639383ad97e5516cd29a3577702d0
rmanuel7 <manuel7@outlook.es>
2025-06-10 21:17:20 -0500

    Create MvcSymbols.js

---
+++ projects/07-mvc-ruter/Shared/MvcSymbols.js
@@ -0,0 +1,5 @@
+export const ROUTE_ELEMENT_TYPE = Symbol('mvc.route');
+export const ROUTES_ELEMENT_TYPE = Symbol('mvc.routes');
+export const CONTROLLER_ELEMENT_TYPE = Symbol('mvc.controller');
+export const AUTHORIZE_ELEMENT_TYPE = Symbol('mvc.authorize');
+export const ALLOWANONYMOUS_ELEMENT_TYPE = Symbol('mvc.allowAnonymous');

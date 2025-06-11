da47265ca3eb4c71c34e17f22bba2ed201b686c4
rmanuel7 <manuel7@outlook.es>
2025-06-10 20:35:36 -0500

    Update Route.js

--- projects/07-mvc-ruter/Router/Route.js
+++ projects/07-mvc-ruter/Router/Route.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+/**
+ * Marcador declarativo para describir una ruta
+ */
 class Route extends React.Component {
     render() {
         // No renderiza nada (estructura declarativa solamente)

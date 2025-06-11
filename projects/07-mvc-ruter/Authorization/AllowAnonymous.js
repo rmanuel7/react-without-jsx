be7c459569855c4926563ee33a90ce918e14b783
rmanuel7 <manuel7@outlook.es>
2025-06-10 22:00:02 -0500

    Update AllowAnonymous.js

--- projects/07-mvc-ruter/Authorization/AllowAnonymous.js
+++ projects/07-mvc-ruter/Authorization/AllowAnonymous.js
@@ -1,5 +1,7 @@
+import { MVCROUTER_ALLOWANONYMOUS_TYPE } from "../Shared/MvcSymbols.js";
 
 class AllowAnonymous extends React.Component {
+    static __typeof = MVCROUTER_ALLOWANONYMOUS_TYPE;
     render() {
         return null;
     }

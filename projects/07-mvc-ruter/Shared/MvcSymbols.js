a25efa5cb9ba6e8bc3c4fb1e74a4c11a96534537
rmanuel7 <manuel7@outlook.es>
2025-06-10 21:31:31 -0500

    Update MvcSymbols.js

--- projects/07-mvc-ruter/Shared/MvcSymbols.js
+++ projects/07-mvc-ruter/Shared/MvcSymbols.js
@@ -1,5 +1,5 @@
-export const ROUTE_ELEMENT_TYPE = Symbol('mvc-router.route');
-export const ROUTES_ELEMENT_TYPE = Symbol('mvc-router.routes');
-export const CONTROLLER_ELEMENT_TYPE = Symbol('mvc-router.controller');
-export const AUTHORIZE_ELEMENT_TYPE = Symbol('mvc-router.authorize');
-export const ALLOWANONYMOUS_ELEMENT_TYPE = Symbol('mvc-router.allowanonymous');
+export const MVCROUTER_ROUTE_TYPE = Symbol('mvc-router.route');
+export const MVCROUTER_ROUTES_TYPE = Symbol('mvc-router.routes');
+export const MVCROUTER_CONTROLLER_TYPE = Symbol('mvc-router.controller');
+export const MVCROUTER_AUTHORIZE_TYPE = Symbol('mvc-router.authorize');
+export const MVCROUTER_ALLOWANONYMOUS_TYPE = Symbol('mvc-router.allowanonymous');

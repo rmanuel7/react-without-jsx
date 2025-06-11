a9afddf0aac00cc612d96599037a6ba92350c7ed
rmanuel7 <manuel7@outlook.es>
2025-06-10 22:00:45 -0500

    Update Authorize.js

--- projects/07-mvc-ruter/Authorization/Authorize.js
+++ projects/07-mvc-ruter/Authorization/Authorize.js
@@ -1,5 +1,7 @@
+import { MVCROUTER_AUTHORIZE_TYPE } from "../Shared/MvcSymbols.js";
 
 class Authorize extends React.Component {
+    static __typeof = MVCROUTER_AUTHORIZE_TYPE;
     render() {
         return null;
     }

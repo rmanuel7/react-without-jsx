b6fa4ef870c17664cab4e5f315217fd169328c8f
rmanuel7 <manuel7@outlook.es>
2025-06-10 22:50:50 -0500

    Create normalizePath.js

---
+++ projects/07-mvc-ruter/Shared/normalizePath.js
@@ -0,0 +1,18 @@
+/**
+ * Normaliza la ruta, evitando errores de comparación por '/' en finales innecesarios.
+ * 
+ * Asegura que todas las rutas: Empiecen con '/' y no terminen con '/' (excepto '/' en sí), y que no tengan '//' duplicados
+ * @param {string} path - Ruta
+ * @returns {string} - Ruta normalizada
+ */
+function normalizePath(path) {
+    if (!path) return '/';
+
+    const startNormalized = ('/' + path);
+    const duplicateNormalized = startNormalized.replace(/\/+/g, '/');
+    const endNormalized = duplicateNormalized.replace(/\/$/, '');
+        
+    return endNormalized || '/';
+};
+
+export default normalizePath;

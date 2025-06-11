8595b801b4973a86e524e6da8876439841a4b09a
rmanuel7 <manuel7@outlook.es>
2025-06-10 21:55:04 -0500

    Update Route.js

--- projects/07-mvc-ruter/Router/Route.js
+++ projects/07-mvc-ruter/Router/Route.js
@@ -1,9 +1,11 @@
-import React from "react";
+import { MVCROUTER_ROUTE_TYPE } from "../Shared/MvcSymbols.js";
 
 /**
+ * Componente Route:
  * Marcador declarativo para describir una ruta
  */
 class Route extends React.Component {
+    static __typeof = MVCROUTER_ROUTE_TYPE;
     render() {
         // No renderiza nada (estructura declarativa solamente)
         return null;

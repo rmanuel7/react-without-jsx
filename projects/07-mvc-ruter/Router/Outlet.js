fe191b0ceeb4abc765826443ed28d46e03c9a127
rmanuel7 <manuel7@outlook.es>
2025-06-11 23:01:02 -0500

    Documentamos la clase Outlet con JSDoc
    Este componente se utiliza dentro de un layout para indicar dónde debe renderizarse la vista hija correspondiente a la ruta actual.

--- projects/07-mvc-ruter/Router/Outlet.js
+++ projects/07-mvc-ruter/Router/Outlet.js
@@ -1,7 +1,25 @@
-// Outlet solo consume el contexto que está “más cerca”.
-import React from "react";
-import OutletContext  from "./OutletContext";
+import { createReactElement as h } from '../Shared/ReactFunctions.js';
+import OutletContext from './OutletContext.js';
 
+/**
+ * Componente Outlet: Marcador de posición para las rutas anidadas.
+ *
+ * Este componente se utiliza dentro de un layout para indicar
+ * dónde debe renderizarse la vista hija correspondiente a la ruta actual.
+ * 
+ * El valor del outlet es proporcionado por el contexto `OutletContext`,
+ * que es llenado dinámicamente por el componente <Routes />.
+ *
+ * @example
+ * // En un layout
+ * class MainLayout extends React.Component {
+ *   render() {
+ *     return h({
+ *       type: Outlet,
+ *     });
+ *   }
+ * }
+ */
 class Outlet extends React.Component {
     render() {
         return h({

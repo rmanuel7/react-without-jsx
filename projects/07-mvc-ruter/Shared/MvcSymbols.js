67311705ea47e374219796fbe1445a7c3f6414f7
rmanuel7 <manuel7@outlook.es>
2025-06-11 23:30:40 -0500

    Update MvcSymbols.js

--- projects/07-mvc-ruter/Shared/MvcSymbols.js
+++ projects/07-mvc-ruter/Shared/MvcSymbols.js
@@ -1,6 +1,36 @@
+// The Symbol used to tag the ReactElement-like types.
+/**
+ * Symbol para marcar la clase o el componente route.
+ * @type {Symbol}
+ */
 export const MVCROUTER_ROUTE_TYPE = Symbol.for('mvc-router.route');
+/**
+ * Symbol para marcar la clase o el componente routes.
+ * @type {Symbol}
+ */
 export const MVCROUTER_ROUTES_TYPE = Symbol.for('mvc-router.routes');
+/**
+ * Symbol para marcar la clase o el componente controlador.
+ * @type {Symbol}
+ */
 export const MVCROUTER_CONTROLLER_TYPE = Symbol.for('mvc-router.controller');
+/**
+ * Symbol para marcar la clase o el componente layout.
+ * @type {Symbol}
+ */
 export const MVCROUTER_LAYOUT_TYPE = Symbol.for('mvc-router.layout');
+/**
+ * Symbol para marcar la clase o el componente view.
+ * @type {Symbol}
+ */
+export const MVCROUTER_VIEW_TYPE = Symbol.for('mvc-router.view');
+/**
+ * Symbol para marcar la clase o el componente authorize.
+ * @type {Symbol}
+ */
 export const MVCROUTER_AUTHORIZE_TYPE = Symbol.for('mvc-router.authorize');
+/**
+ * Symbol para marcar la clase o el componente allowAnonnymous.
+ * @type {Symbol}
+ */
 export const MVCROUTER_ALLOWANONYMOUS_TYPE = Symbol.for('mvc-router.allowanonymous');

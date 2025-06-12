0e13b34e10abc278369387078c3cf0560fbc2bed
rmanuel7 <manuel7@outlook.es>
2025-06-11 21:19:32 -0500

    Añadimos la documentación JSDoc a la declaración de tu AuthzContext
    Contexto para acceder a los datos y funciones relacionadas con la autorización y autenticación global.

--- projects/07-mvc-ruter/Authorization/AuthzContext.js
+++ projects/07-mvc-ruter/Authorization/AuthzContext.js
@@ -1,4 +1,13 @@
-
+/**
+ * Contexto para acceder a los datos y funciones relacionadas con la autorización y autenticación global.
+ *
+ * @type {React.Context<AuthContextValue>}
+ * @description
+ * Permite a los componentes acceder y modificar el estado de autenticación y autorización de manera global.
+ * Este contexto debe ser provisto por un `AuthzContext.Provider` en un nivel superior del árbol de componentes.
+ * @remark Todos los consumidores que sean descendientes del `AuthzContext.Provider`
+ * volverán a renderizar cada vez que cambie el `value` del contexto.
+ */
 const AuthzContext = React.createContext(null);
 
 export default AuthzContext;

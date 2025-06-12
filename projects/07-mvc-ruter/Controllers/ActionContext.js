4bc9a08249b3238694166588a9e315828e8c28a9
rmanuel7 <manuel7@outlook.es>
2025-06-11 21:49:29 -0500

    Update ActionContext.js

--- projects/07-mvc-ruter/Controllers/ActionContext.js
+++ projects/07-mvc-ruter/Controllers/ActionContext.js
@@ -1,5 +1,14 @@
-import React from "react";
-
+/**
+ * Contexto para manejar la información de la acción global en la aplicación.
+ *
+ * @type {React.Context<ActionContextValue>}
+ * @description
+ * Este contexto permite a los componentes acceder y modificar el estado de la acción actual
+ * de manera global. Es útil para flujos donde múltiples componentes necesitan reaccionar o
+ * interactuar con una acción en curso (por ejemplo, el envío de un formulario, una operación de guardado).
+ * @remark Todos los consumidores que sean descendientes del `ActionContext.Provider`
+ * volverán a renderizar cada vez que cambie el `value` del contexto.
+*/
 const ActionContext = React.createContext(null);
 
 export default ActionContext;

865f23ec722a6e8116c4113111ba9adb838b7cf3
rmanuel7 <manuel7@outlook.es>
2025-06-11 23:13:25 -0500

    Documentamos la clase Routes con JSDoc
    Contenedor para la definición de rutas anidadas.

--- projects/07-mvc-ruter/Router/Routes.js
+++ projects/07-mvc-ruter/Router/Routes.js
@@ -1,20 +1,28 @@
-import { MVCROUTER_ROUTES_TYPE } from "../Shared/MvcSymbols.js";
-import { createReactElement as h } from '../Shared/ReactFunction.js';
-import OutletContext from "./OutletContext.js";
-import Outlet from "./Outlet.js";
-// Importa tus wrappers y componentes de rutas
-import Authorize from "../Authorization/Authorize.js";
-import AllowAnonymous from "../Authorization/AllowAnonymous.js";
-import Controller from "../Controllers/Controller.js";
-import Route from "./Route.js";
+import { MVCROUTER_ROUTES_TYPE } from '../Shared/MvcSymbols.js';
+import { createReactElement as h } from '../Shared/ReactFunctions.js';
+import { cloneReactElement as o } from '../Shared/ReactFunctions.js';
+import Authorization from '../Authorization/Authorization.js';
+import AuthzContext from '../Authorization/AuthzContext.js';
+import NotFound from './NotFound.js';
+import OutletContext from './OutletContext.js';
+import withRouter from './withRouter.js';
+import matchRoute from './matchRoute.js';
+import ActionContext from '../Controllers/ActionContext.js';
 
 /**
  * Componente Routes:
  * Hace matching recursivo, construye la pila de wrappers y renderiza el outlet final usando contextos.
  */
 class Routes extends React.Component {
-    static __typeof = MVCROUTER_ROUTES_TYPE;
-    
+    /**
+    * Propiedad estática para identificar este componente como un tipo de rutas
+    * dentro del sistema de enrutamiento (ej. para props.element en Route).
+    * @returns {Symbol} Un símbolo único que representa el tipo de componente de rutas.
+    */
+    static get __typeof() {
+        return MVCROUTER_ROUTES_TYPE;
+    }
+
     render() {
         const { children, location } = this.props;
 
@@ -32,22 +40,22 @@ class Routes extends React.Component {
         // Armado del pipeline de contextos y wrappers
         // 1. Layout (definición, no ejecuta aún)
         const layout = contextValues.layout
-          ? h({
-                type: contextValues.layout.type,
-                props: contextValues.layout.props
+            ? o({
+                element: contextValues.layout.element,
+                config: contextValues.layout.props
             })
-          : null;
+            : null;
 
         // 2. Action pipeline: OutletContext + ActionContext + ActionResult
         const action = h({
             type: OutletContext.Provider,
-            props: { value: { layout } },
+            props: { value: { outlet: layout } },
             children: [
                 h({
                     type: ActionContext.Provider,
                     props: { value: contextValues.routeMatch || {} },
                     children: [
-                        h({ type: ActionResult })
+                        o({ element: contextValues.routeMatch.controller.element || contextValues.routeMatch.action.element })
                     ]
                 })
             ]
@@ -56,14 +64,19 @@ class Routes extends React.Component {
         // 3. Authorization pipeline
         let element = action;
         if (contextValues.authorization) {
+            // 4. `Authorization` wrapper que decide si deja pasar, y provee el siguiente `Outlet` (la `action`).
+            // `Authorization` consume `AuthzContext` para la decisión.
+            // `Authorization` consume `OutletContext` para obtener la `action` que debe renderizar.
             element = h({
-                type: AuthzContext.Provider,
-                props: { value: contextValues.authorization || {} },
+                type: OutletContext.Provider, // Provee el `actionElement` al `Authorization`
+                props: { value: { outlet: element } },
                 children: [
                     h({
-                        type: Authorization,
-                        props: contextValues.authorization.config || {},
-                        children: element
+                        type: AuthzContext.Provider, // Provee los datos de autorización
+                        props: { value: contextValues.authorization || {} },
+                        children: [
+                            h({ type: Authorization }) // `Authorization` es el primer componente en el pipeline
+                        ]
                     })
                 ]
             });
@@ -73,4 +86,4 @@ class Routes extends React.Component {
     }
 }
 
-export default Routes;
+export default withRouter(Routes);

6f64abef07d27d10b26c7c82cf3a6230a623280d
Robinson Batista <manuel7@outlook.es>
2025-07-01 18:08:19 -0500

    Update Host.js

--- projects/10-hosting/Host.js
+++ projects/10-hosting/Host.js
@@ -2,9 +2,11 @@ import HostingSymbols from './HostingSymbols.js';
 import HostEnvironment from './HostEnvironment.js';
 import HostOptions from './HostOptions.js';
 import HostApplicationLifetime from './HostApplicationLifetime.js';
-import { LoggerFactory } from '@spajscore/logging';
 import { Logger } from '@spajscore/logging';
 import ConsoleLifetime from './ConsoleLifetime.js';
+import { ServiceProvider } from '@spajscore/dependency-injection';
+import { CancellationToken, CancellationTokenSource } from '@spajscore/threading';
+import { EnumerableTemplate } from '@spajscore/collections';
 
 /**
  * Host
@@ -33,105 +35,144 @@ class Host {
         return HostingSymbols.host;
     }
 
-    /** * Metadatos para el contenedor DI.
-     * Define las dependencias requeridas y el
-     * tipo que provee este host.
+    /**
+     * Metadatos para inyección de dependencias.
      * @returns {object}
-     * @property {symbol[]} provides - Tipos que este host provee.
-     * @property {object} inject - Dependencias requeridas por este host.
-     * @property {HostEnvironment} inject.environment - Entorno de ejecución.
-     * @property {HostOptions} inject.options - Opciones de configuración del host.
-     * @property {HostApplicationLifetime} inject.applicationLifetime - Ciclo de vida de la aplicación.
-     * @property {LoggerFactory} inject.loggerFactory - Fábrica de loggers para registrar eventos.
      */
     static get __metadata() {
         return {
-            provides: [Host.__typeof],
+            parameters: [],
+            properties: {},
             inject: {
+                services: ServiceProvider,
                 environment: HostEnvironment,
                 options: HostOptions,
                 applicationLifetime: HostApplicationLifetime,
                 consoleLifetime: ConsoleLifetime,
-                loggerFactory: LoggerFactory
+                logger: HostingSymbols.makeTypeBySymbol(HostingSymbols.forTypeBySymbol(this.__typeof, Logger.__typeof))
             }
         };
     }
 
-    /** @type {HostEnvironment} */        #environment;
-    /** @type {HostOptions} */            #options;
-    /** @type {HostApplicationLifetime} */#applicationLifetime;
+    /** @type {Logger<Host>} */           #logger;
     /** @type {ConsoleLifetime} */        #consoleLifetime;
-    /** @type {Logger} */                 #logger;
-    /** @type {boolean} */                #started;
+    /** @type {HostApplicationLifetime} */#applicationLifetime;
+    /** @type {HostOptions} */            #options;
+    /** @type {HostEnvironment} */        #environment;
+    /** @type {ServiceProvider} */        #services;
+
+    /** @type {Array<HostedService>|null} */ #hostedServices;
+    /** @type {Array<HostedLifecycleService>|null} */ #hostedLifecycleServices;
+    /** @type {boolean} */                #hostStarting;
+    /** @type {boolean} */                #hostStopped;
 
     /**
      * @param {object} deps
+     * @param {ServiceProvider} deps.services
      * @param {HostEnvironment} deps.environment
      * @param {HostOptions} deps.options
      * @param {HostApplicationLifetime} deps.applicationLifetime
      * @param {ConsoleLifetime} deps.consoleLifetime
-     * @param {LoggerFactory} deps.loggerFactory
+     * @param {Logger<Host>} deps.logger
      */
-    constructor({ environment, options, applicationLifetime, consoleLifetime, loggerFactory }) {
+    constructor({ services, environment, options, applicationLifetime, consoleLifetime, logger }) {
+        if (!services || services instanceof ServiceProvider === false) {
+            throw new Error('HostBuilder: operación invalida. El ServiceProvider es requerido.')
+        }
+        if (!applicationLifetime || applicationLifetime instanceof HostApplicationLifetime === false) {
+            throw new Error('HostBuilder: operación invalida. El HostApplicationLifetime es requerido.')
+        }
+        if (!consoleLifetime || consoleLifetime instanceof ConsoleLifetime === false) {
+            throw new Error('HostBuilder: operación invalida. El ConsoleLifetime es requerido.')
+        }
+        if (!logger || logger instanceof Logger === false) {
+            throw new Error('HostBuilder: operación invalida. El Logger es requerido.')
+        }
+        this.#services = services;
         this.#environment = environment;
         this.#options = options;
         this.#applicationLifetime = applicationLifetime;
         this.#consoleLifetime = consoleLifetime;
-        this.#logger = loggerFactory.createLogger(Symbol.keyFor(Host.__typeof));
-        this.#started = false;
+        this.#logger = logger;
+        this.#hostStarting = false;
     }
 
     /**
      * Devuelve el entorno de ejecución.
-     * @returns {HostEnvironment}
+     * @returns {ServiceProvider}
      */
-    get environment() {
-        return this.#environment;
-    }
-
-    /**
-     * Devuelve las opciones del host.
-     * @returns {HostOptions}
-     */
-    get options() {
-        return this.#options;
-    }
-
-    /**
-     * Devuelve el ciclo de vida de la aplicación.
-     * @returns {HostApplicationLifetime}
-     */
-    get applicationLifetime() {
-        return this.#applicationLifetime;
-    }
-    /**
-     * Devuelve el ciclo de vida de la consola.
-     * @returns {ConsoleLifetime}
-     */
-    get consoleLifetime() {
-        return this.#consoleLifetime;
-    }
-
-    /**
-     * Indica si el host está iniciado.
-     * @returns {boolean}
-     */
-    get isStarted() {
-        return this.#started;
+    get services() {
+        return this.#services;
     }
 
     /**
      * Inicia el host y el ciclo de vida.
+     * @descriptions
+     * Order:
+     * - IHostLifetime.WaitForStartAsync
+     * - Services.GetService{IStartupValidator}().Validate()
+     * - IHostedLifecycleService.StartingAsync
+     * - IHostedService.Start
+     * - IHostedLifecycleService.StartedAsync
+     * - IHostApplicationLifetime.ApplicationStarted
+     * @param {CancellationToken} cancellationToken 
      * @returns {Promise<void>}
      */
-    async start() {
-        if (this.#started) return;
-        this.#logger.info('Host starting...');
-        // Aquí puedes agregar lógica de inicialización si es necesario
-        this.#consoleLifetime.start();
-        this.#applicationLifetime.start();
-        this.#started = true;
-        this.#logger.info('Host started.');
+    async start(cancellationToken) {
+        this.#logger.info('Hosting starting...');
+
+        // Análogo a `using (var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _applicationLifetime.ApplicationStopping))`
+        const internalCancellationTokenSource = CancellationTokenSource.createLinkedTokenSource(
+            cancellationToken || new CancellationToken(new AbortController().signal), // Un token vacío si no hay uno entrante
+            this.#applicationLifetime.applicationStopping
+        );
+
+
+        if (this.#options.startupTimeout !== Infinity) {
+            internalCancellationTokenSource.cancelAfter(this.#options.startupTimeout);
+        }
+
+        cancellationToken = internalCancellationTokenSource.token;
+
+        const exceptions = [];
+        let hostStarting = false;
+
+        try {
+            // ConsoleLifetime se "inicia" configurando sus listeners.
+            await this.#consoleLifetime.waitForStartAsync(cancellationToken);
+            // Verifica cancelación después de iniciar listeners
+            cancellationToken.throwIfCancellationRequested();
+
+            const concurrent = this.#options.servicesStartConcurrently;
+            const abortOnFirstException = !concurrent;
+            const EnumerableOfHosted = EnumerableTemplate.forType(HostingSymbols.makeTypeBySymbol(HostingSymbols.hostedService));
+            this.#hostedServices ??= this.#services.get(EnumerableOfHosted);
+            // this.#hostedLifecycleServices = GetHostLifecycles(_hostedServices);
+
+            hostStarting = true;
+
+            for (const svc of this.#hostedServices) {
+                if (concurrent) {
+                    svc.start(cancellationToken);
+                }
+                else {
+                    await svc.start(cancellationToken);
+                }
+            }
+
+            // Call IHostApplicationLifetime.Started
+            // This catches all exceptions and does not re-throw.
+            this.#applicationLifetime.notifyStarted();
+
+            this.#logger.info('Hosting started')
+        }
+        catch (error) {
+            this.#logger.error('Hosting failed', error)
+            throw error
+        }
+        finally {
+            this.#hostStarting = false;
+        }
     }
 
     /**
@@ -139,13 +180,13 @@ class Host {
      * @returns {Promise<void>}
      */
     async stop() {
-        if (!this.#started) return;
+        if (!this.#hostStarting) return;
         this.#logger.info('Host stopping...');
         // Aquí puedes agregar lógica de limpieza si es necesario
         this.#consoleLifetime.stop();
         this.#applicationLifetime.stop();
         // Aquí puedes agregar lógica de espera o limpieza si es necesario
-        this.#started = false;
+        this.#hostStarting = false;
         this.#logger.info('Host stopped.');
     }
 }
